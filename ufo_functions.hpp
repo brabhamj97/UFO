bcc958a7799ce29e8807f2b1595f80fd5818033c
Jack <92158932+brabhamj97@users.noreply.github.com>
2023-01-04 08:23:57 +0000

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+
+void display_misses(int misses);
+void greet();
+void endgame(std::string answer, std::string codeword);
+void display_status(std::vector<char>, std::string answer);
